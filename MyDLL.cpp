ca06af1160f5a8155a4f22d2d1039394ca908fee
Liye <wangliye1985@163.com>
2018-10-29 19:33:08 +0800

    Update MyDLL.cpp

--- MyDLL.cpp
+++ MyDLL.cpp
@@ -3,8 +3,28 @@
 
 extern "C" {
 	_declspec(dllexport) int add(int a, int b);
+	_declspec(dllexport) int divide(int a, int b, int* c);
+	_declspec(dllexport) double avg(double* dptr, int len);
 }
 
-_declspec(dllexport) int add(int a, int b){
-  return a + b;
+_declspec(dllexport) int add(int a, int b)
+{
+	return a + b;
+}
+
+_declspec(dllexport) int divide(int a, int b, int* c)
+{
+	int rst = a / b;
+	*c = a % b;
+	return rst;
+}
+
+_declspec(dllexport) double avg(double* dptr, int len)
+{
+	double total = 0.0;
+	for(int i = 0; i < len; ++i)
+	{
+		total += dptr[i];
+	}
+	return total / len;
 }
