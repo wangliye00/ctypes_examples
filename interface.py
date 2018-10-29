33df517b36c4f7117300b42808d8e17f8e26c19d
Liye <wangliye1985@163.com>
2018-10-29 15:59:10 +0800

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,5 +1,8 @@
 import ctypes
 
+#! When call shared library, you should define datatype of args by argtypes, \
+#! and data type of return value by restype. Set restype to None if there's no return value.
+
 def main():
     lib = ctypes.cdll.LoadLibrary('MyDLL.dll')
     add = lib.add
